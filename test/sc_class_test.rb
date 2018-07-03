840021ab5f0f917a4752237fba05ceebfcf3da05
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-07-03 17:28:24 -0600

    dis werk?

--- test/sc_class_test.rb
+++ test/sc_class_test.rb
@@ -1,13 +1,13 @@
 # Sorting Card test file?
 
 require "pry"
-require "./lib/sorting_cards.rb"
+require "../lib/sorting_cards_card_class.rb"
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class SortingCardTest < Minitest::Test
-  def test_correct_value
+  def test_correct_card_value
     card = Card.new("Ace","Spades")
-    assert_equal "Ace" , card.get_value
+    assert_equal "Ace", card.value
   end
 end
