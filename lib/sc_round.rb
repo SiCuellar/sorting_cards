8e2983636fa36f79f20c5e80b5b1a737a9059511
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-07-04 12:16:58 -0600

    create round class

---
+++ lib/sc_round.rb
@@ -0,0 +1,42 @@
+require "pry"
+require "./lib/sc_round.rb"
+require "./lib/sc_card_class.rb"
+require "./lib/sc_guess_class.rb"
+
+
+class Round
+
+attr_reader :deck,
+            :guesses
+
+
+
+  def initialize(deck)
+    @deck = deck
+    @guesses = []
+  end
+
+  def current_card
+    @deck.cards.first
+  end
+
+  def record_guess(response)
+    @guesses << Guess.new(response, current_card)
+    @deck.cards.rotate!
+  end
+
+  def number_correct
+    number_correct = 0
+    @guesses.map do |guess|
+      if guess.correct? == true
+        number_correct += 1
+      end
+    end
+    number_correct
+  end
+
+  def percent_correct
+    (number_correct * 100) / @guesses.length
+  end
+
+end
