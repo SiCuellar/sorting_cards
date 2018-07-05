3f1fe8fb595dd0c0b0a901fb272bef81608a2609
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-07-04 21:05:19 -0600

    creates almost fucntioning version of iteration three

--- lib/sc_deck.rb
+++ lib/sc_deck.rb
@@ -4,15 +4,91 @@ require "./lib/sc_guess_class.rb"
 
 class Deck
 
-  attr_reader :cards_array
+  attr_reader :cards
 
-  def initialize(input_card_array)
-    @cards_array = input_card_array
+  def initialize(cards)
+    @cards = cards
   end
 
   def count
-    @cards_array.count
+    @cards.count
 
   end
 
+
+
+
+
+
+  def sort
+
+    card_worth = {"2" => 2,
+                  "3" => 3,
+                  "4" => 4,
+                  "5" => 5,
+                  "6" => 6,
+                  "7" => 7,
+                  "8" => 8,
+                  "9" => 9,
+                  "10" => 10,
+                  "Jack" => 11,
+                  "Queen" => 12,
+                  "King" => 13,
+                  "Ace" => 14 ,
+                  "Clubs" => 0.1,
+                  "Diamonds" => 0.2,
+                  "Hearts" => 0.3,
+                  "Spades" => 0.4
+                  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    array_count = @cards.length
+    loop do
+      swapped= false
+
+
+      (array_count - 1).times do |card|
+        if card_worth[@cards[card].value] > card_worth[@cards[card+1].value]
+          @cards[card], @cards[card+1] = @cards[card+1], @cards[card]
+          swapped = true
+
+        elsif card_worth[@cards[card].value] == card_worth[@cards[card+1].value]
+            card_a = card_worth[@cards[card].value] + card_worth[@cards[card].suit]
+            card_b = card_worth[@cards[card+1].value] + card_worth[@cards[card+1].suit]
+            if card_a > card_b
+              card_a, card_b = card_b, card_a
+              swapped = true
+            end
+
+
+
+
+
+
+
+        end
+      end
+      # binding.pry
+      break if not swapped
+    end
+
+
+    @cards
+
+  end
 end
